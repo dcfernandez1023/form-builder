4911507c4b86b388db7d22d0d634b5e995b9352c
Dominic Fernandez <dominic.fernandez1023@gmail.com>
2022-01-11 23:38:11 -0800

    Heroku

--- frontend/src/controllers/constants.js
+++ frontend/src/controllers/constants.js
@@ -2,5 +2,5 @@ console.log(process.env.ENVIRONMENT);
 
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
 export const ACCESS_TOKEN_HEADER = "access-token";
-export const SERVER_API_URL = process.env.ENVIRONMENT === "prod" ? window.location.protocol+'//'+ window.location.hostname + (window.location.port.toString().trim().length !== 0 ? ':'+ window.location.port: '') : "http://localhost:5000";
+export const SERVER_API_URL = process.env.REACT_SERVER_ENVIRONMENT === "prod" ? window.location.protocol+'//'+ window.location.hostname + (window.location.port.toString().trim().length !== 0 ? ':'+ window.location.port: '') : "http://localhost:5000";
 export const TOKEN_NOT_FOUND = "Token not found";
